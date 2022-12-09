f293538a3c744fd04e86a568004ec6df352b3b05
Christian LeVesque <christian@levesque.dev>
2022-12-09 17:31:03 -0600

    Changed services to use ServiceResult<T> instead of using object, to improve type mapping across API and client

--- Services/ICrudService.cs
+++ Services/ICrudService.cs
@@ -13,34 +13,34 @@ public interface ICrudService<TDto, TKey>
 	/// </summary>
 	/// <param name="model">The DTO representing the entity to create</param>
 	/// <returns><c>Task</c></returns>
-	Task<ServiceResult> Add(TDto model);
+	Task<ServiceResult<TKey>> Add(TDto model);
 
 	/// <summary>
 	/// Updates an existing entity in the backend
 	/// </summary>
 	/// <param name="model">The DTO representing the entity to update</param>
 	/// <returns><c>Task</c></returns>
-	Task<ServiceResult> Edit(TDto model);
+	Task<ServiceResult<bool>> Edit(TDto model);
 
 	/// <summary>
 	/// Gets an entity by primary key
 	/// </summary>
 	/// <param name="id">The primary key of the entity to retrieve</param>
 	/// <returns>the requested <c>TModel</c></returns>
-	Task<ServiceResult> Get(TKey id);
+	Task<ServiceResult<TDto>> Get(TKey id);
 
 	/// <summary>
 	/// Gets a list of all entities in the backend
 	/// </summary>
 	/// <returns>a list of all entities in the database</returns>
-	Task<ServiceResult> Get(Filter? filter = null);
+	Task<ServiceResult<PagedDto<TDto>>> Get(Filter? filter = null);
 
 	/// <summary>
 	/// Deletes an entity by primary key
 	/// </summary>
 	/// <param name="id">The primary key of the entity to delete</param>
 	/// <returns><c>Task</c></returns>
-	Task<ServiceResult> Delete(TKey id);
+	Task<ServiceResult<bool>> Delete(TKey id);
 }
 
 public interface ICrudService<TDto> : ICrudService<TDto, Guid>
